356863fc747e5300fae3488b14459a51976fe007
danielbarros01 <danibelgranocab@hotmail.com>
2023-03-11 18:23:30 -0300

    fix error in connection

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -8,10 +8,13 @@ const options = {
   logging: config.isProd ? false : true,
 }
 
-if(config.isProd){
-  options.ssl = {
-    rejectUnauthorized: false
+if (config.isProd) {
+  options.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
+
 }
 
 const sequelize = new Sequelize(config.dbUrl, options);
